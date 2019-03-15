11be58982ecd7d57bc958d3c876665f8a1493397
congrongdeshuo <17698567339@163.com>
2019-03-15 17:29:50 +0800

    add store

---
+++ src/store/mutations.js
@@ -0,0 +1,21 @@
+const ADD_ITEMNUM = 'ADD_ITEMNUM'
+const REMBER_ANSWER = 'REMBER_ANSWER'
+const REMBER_TIME = 'REMBER_TIME'
+const INITIALIZE_DATA = 'INITIALIZE_DATA'
+export default {
+	//点击进入下一题
+	[ADD_ITEMNUM](state, payload) {
+		state.itemNum += payload.num;
+	},
+	//记录答案
+	[REMBER_ANSWER](state, payload) {
+		state.answerid[state.itemNum] = payload.id;
+	},
+	/*
+	初始化信息，
+	 */
+	[INITIALIZE_DATA](state) {
+		state.itemNum = 1;
+		state.allTime = 0;
+	},
+}
